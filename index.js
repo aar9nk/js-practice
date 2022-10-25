16f3e90d24b66cb04b955438460b5349a7266ec9
Aaron Byrom <aaronbyrom@protonmail.com>
2022-10-25 15:52:17 +1100

    add base project

---
+++ index.js
@@ -0,0 +1,23 @@
+// 1. Write a function that greets the user based on a variable
+
+// 1b. Write a function that greets the user based on a parameter
+
+// 2. Write a function that adds two numbers together based on a variable
+
+// 2b. Write a function that adds two numbers together based on a parameter
+
+// 3. Write a function that converts as number to a string
+
+// 4. Write a function that will log either "yes" or "no" when given a boolean
+
+// 5. Write a function that will check if the user is at least 18 years old
+
+// 5b. Write a function that will check if the user is at least 18 years old 
+// and log the result using the function you created above
+
+// 6. Write a function that returns a string with no white spaces at the start
+//  and end of the string.
+// "  Hi, my name is aaron    " -> "Hi, my name is aaron"
+
+// 7. Write a function that returns an abbreviated name.
+// Aaron Byrom -> AB
