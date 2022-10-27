4438140cbe4060217bc13d52426c34ac0b389ed0
Aaron Byrom <aaronbyrom@protonmail.com>
2022-10-27 17:34:50 +1100

    phase 2

---
+++ loop-practice.js
@@ -0,0 +1,15 @@
+//1. You get an array of numbers, return the sum of all numbers.
+// Example [1,7,12] => 1 + 7 + 12 = 20
+// [10,5,9,13,1]
+
+// 2. create a function that removes the first and last characters of a string
+// 'Hello, World!'
+
+// 3. Create a function that checks if a user's input is a capitol city of Australia
+// [Brisbane, Darwin, Hobart, Melbourne, Perth, Sydney, Canberra, Adelaide]
+
+// 4. Given a list of users, check they are all over 18
+// [18,21,15,30,60,2]
+
+// 5. find the average of an array
+// [18,21,15,30,60,2]
