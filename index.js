be13b053f76106220791fbe1f890ad08ff721063
Aaron Byrom <aaronbyrom@protonmail.com>
2022-11-01 20:38:59 +1100

    start

---
+++ index.js
@@ -0,0 +1,21 @@
+// 1. fiind the average of an array
+// const array = [10, 20, 30, 40, 50];
+
+// function findAverage(input) {
+//   let result = 0;
+
+//   for (let i = 0; i < input.length; i++) {
+//     result = result + input[i];
+//   }
+
+//   return result / input.length;
+// };
+
+// console.log(findAverage(array));
+
+//2. find if the user's input exists in our array
+const array = ['aaron', 'raquel', 'jeannie', 'benita', 'roy'];
+
+const userInput1 = 'jeannie';
+const userInput2 = 'Hello, World!';
+const userInput3 = 'Roy';
